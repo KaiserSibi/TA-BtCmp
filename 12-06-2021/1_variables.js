1da6e343a695849188bdc19c8cee96f2b731897a
Kaiser Sibi <62590962+KaiserSibi@users.noreply.github.com>
2022-04-25 08:46:21 +0800

    Add files via upload

---
+++ 12-06-2021/1_variables.js
@@ -0,0 +1,8 @@
+// Quick tasks:
+// 1. Declare a variable that will be assigned a value which is a unique identifier of a person 
+// at later point in time.
+
+// 2. Declare a variable that stores a value required to convert minutes to seconds.
+
+var uuid;
+var minutes, seconds;
